25298d5bec97bb2535a55fcf7f05484b82c0e8f4
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-12-16 04:05:34 +0900

    Update template.cpp, Remove unsolved problems

--- template.cpp
+++ template.cpp
@@ -16,14 +16,22 @@ using namespace std;
 
 using ll = long long;
 using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vstr = vector<string>;
 
 #define ALL(x) x.begin(), x.end()
+#define IALL(x) x.rbegin(), x.rend()
 #define FOR(i, x, y) for (int i = x; i < y; i++)
 #define FOR_(i, x, y) for (int i = x; i <= y; i++)
 #define IFOR(i, x, y) for (int i = y; i > x; i--)
 #define IFOR_(i, x, y) for (int i = y; i >= x; i--)
 #define REP(i, x) FOR(i, 0, x)
-#define DEBUG(x) cerr << "\033[1;35m" << x << "\033[0m\n"
+#define SUBSTR(s, l, r) s.substr(l, r - l + 1)
+#define RESET(arr, x) memset(arr, x, sizeof(arr))
+#define PRINT(x) cout << (x) << ' '
+#define PRINTLN(x) cout << (x) << '\n'
+#define DEBUG(x) cerr << "\033[1;35m" << (x) << "\033[0m\n"
 
 template<typename T>
 void chmax(T &m, const T q) { m = max(m, q); }
@@ -55,6 +63,18 @@ T manhattan(pair<T, T> a, pair<T, T> b) { return abs(b.first - a.first) + abs(b.
 
 template<typename T>
 T euclidean(pair<T, T> a, pair<T, T> b) { return square(b.first - a.first) + square(b.second - a.second); }
+
+// CONSTANTS
+
+#define INF 987654321
+#define UNKNOWN -1
+
+const int dy[]{0, -1, 0, 1, -1, -1, 1, 1};
+const int dx[]{-1, 0, 1, 0, -1, 1, -1, 1};
+
+// FUNCTIONS
+
+
 
 int main() {
     //ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
